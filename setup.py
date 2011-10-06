2552b72bcb1bcc669a8d7e5396f53c512c575823
Gehan Gonsalkorale <gehan.g@gmail.com>
2011-10-06 09:07:34 +0100

    Version number bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-debug-toolbar-mongo',
-    version='0.1.1',
+    version='0.1.2',
     description='MongoDB panel for the Django Debug Toolbar',
     long_description=open('README.rst').read(),
     author='Harry Marr',
