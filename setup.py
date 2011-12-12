2e93285567239e118c6dc6195707c2823b01469f
Colin Howe <colinthehowe@gmail.com>
2011-12-12 11:02:13 +0000

    Version bump#

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-debug-toolbar-mongo',
-    version='0.1.3',
+    version='0.1.4',
     description='MongoDB panel for the Django Debug Toolbar',
     long_description=open('README.rst').read(),
     author='Harry Marr',
